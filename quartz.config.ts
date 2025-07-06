86c2c3ee6015570a51acb7e7f9ad3728d0427de0
Trash King <reedtakesemail@gmail.com>
2025-07-05 21:26:10 -0700

    gonna see if this works

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "retron2000regular",
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
