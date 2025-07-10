94ffd7b4f9f2388f6e5f16581228454cba2537b6
Trash King <reedtakesemail@gmail.com>
2025-07-10 02:27:48 -0700

    adding custom navbar header and fixing margin to 100 on sitebody

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -271,7 +271,7 @@ export function renderPage(
         {/* Mobile navigation */}
         <div className="topnav" id="myTopnav">
           <a href="/index.html" className="active">
-            <img src="/mobileheader.gif" alt="" />
+            <img src="/assets/mobileheader.gif" alt="" />
             <div className="rainbow">The Possum Den</div>
           </a>
 
