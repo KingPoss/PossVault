b508d350d5f6511d341c4d482dec7e897f36bf1a
Trash King <reedtakesemail@gmail.com>
2025-07-12 01:39:54 -0700

    modify defaultdatetype to modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "kb.kingposs.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     generateSocialImages: true,
     theme: {
       fontOrigin: "local",
